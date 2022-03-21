b81a0b1e804ef13def2acfa10e032e3cc1cf2e0b
anarosa07 <102010644+anarosa07@users.noreply.github.com>
2022-03-21 01:47:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.listen(process.env.PORT || 5000);
 app.get('/', (request, response)=>{
 	response.sendFile(path.join(__dirname + "/index.html")); 
-app.listen(process.env.PORT || 5000);
 });
