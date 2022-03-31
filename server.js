d97c4fc2d6dd33dcc09b1a75b1c04536022f17b2
anarosa07 <102010644+anarosa07@users.noreply.github.com>
2022-03-30 22:13:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const portNumber = server.address().port;
-console.log('Puerto activo')
+
 
 app.use(express.static(__dirname + "/raiz"));
 app.get('/', (req, res)=>{
