6b778900a3342787f37ef807d51eb3be55ec30b0
anarosa07 <102010644+anarosa07@users.noreply.github.com>
2022-03-30 22:16:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-
-
+//Enlace entre exprees y css para qué visualice su formato.
 app.use(express.static(__dirname + "/raiz"));
 app.get('/', (req, res)=>{
 	res.sendFile(path.join(__dirname + "/index.html")); 
